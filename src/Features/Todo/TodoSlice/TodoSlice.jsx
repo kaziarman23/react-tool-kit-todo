2c31b4333c9cba1fc2ac4a221ef889fa058172a5
kaziarman23 <kaziarman@proton.me>
2024-05-26 12:44:25 +0600

    project task is complite alhamdulillah

--- src/Features/Todo/TodoSlice/TodoSlice.jsx
+++ src/Features/Todo/TodoSlice/TodoSlice.jsx
@@ -1,7 +1,7 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { nanoid, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{ id: 1, text: "Todo App in Rudex Tool Kit" }],
+    todos: [{ id: 1, text: "re-create-todo" }],
 };
 
 export const todoSlice = createSlice({
@@ -16,11 +16,49 @@ export const todoSlice = createSlice({
             state.todos.push(todo);
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) =>  todo.id !== action.payload);
+            state.todos = state.todos.filter(
+                (todo) => todo.id !== action.payload
+            );
+        },
+        updateTodo: (state, action) => {
+            const {id, text} = action.payload
+            const result = state.todos.find((todo) => todo.id === id)
+            if(result){
+                result.id = id
+                result.text = text
+
+            }
         },
     },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
+
+// import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+// const initialState = {
+//     todos: [{ id: 1, text: "Todo App in Rudex Tool Kit" }],
+// };
+
+// export const todoSlice = createSlice({
+//     name: "todo",
+//     initialState,
+//     reducers: {
+//         addTodo: (state, action) => {
+//             const todo = {
+//                 id: nanoid(),
+//                 text: action.payload,
+//             };
+//             state.todos.push(todo);
+//         },
+//         removeTodo: (state, action) => {
+//             state.todos = state.todos.filter((todo) =>  todo.id !== action.payload);
+//         },
+//     },
+// });
+
+// export const { addTodo, removeTodo } = todoSlice.actions;
+
+// export default todoSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./App/Store.js";
+import { store } from "./App/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
